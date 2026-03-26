a00d24636489a88dff36e2189fa05c2fcff6d36c
arpitaghatge28-collab <arpitaghatge28@gmail.com>
2026-03-26 09:43:39 +0530

    coded uc7

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,21 +2,13 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Case 1: No arguments
+        String names;
+
         if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+            names = "World";
+        } else {
+            names = String.join(", ", args);
         }
-
-        // Case 2: Build names
-        StringBuilder nameBuilder = new StringBuilder();
-
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        // Remove trailing ", "
-        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
 
         System.out.println("Hello, " + names + "!");
     }
