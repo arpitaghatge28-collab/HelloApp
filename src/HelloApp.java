83d6623dea68825f0c4e8bcd7ec6dcea1d541af3
arpitaghatge28-collab <arpitaghatge28@gmail.com>
2026-03-26 09:41:41 +0530

    coded uc6

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,32 +1,23 @@
-/**
- * UC5: Display "Hello" with Multiple Command-Line Arguments
- * using Enhanced For Loop or Default Message
- */
-
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        String name = "World";
-
-        if (args.length > 0) {
-
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-
-            for (String n : args) {
-
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-
-                nameBuilder.append(n);
-                first = false;
-            }
-
-            name = nameBuilder.toString();
+        // Case 1: No arguments
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
-        System.out.println("Hello, " + name + "!");
+        // Case 2: Build names
+        StringBuilder nameBuilder = new StringBuilder();
+
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        // Remove trailing ", "
+        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+        System.out.println("Hello, " + names + "!");
     }
 }
